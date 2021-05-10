9f3a45061ebb324b452cdbc58ab7f85e13334779
marteresagh <marteresa28@gmail.com>
2021-05-10 10:50:27 +0200

    add AABB struct and functions

--- src/AABB.jl
+++ src/AABB.jl
@@ -13,3 +13,45 @@ mutable struct AABB
 		return AABB(min,max)
 	end
 end
+
+
+function update!(aabb::AABB,point::Common.Point)
+	x, y, z = point
+	aabb.min[1] = min(aabb.min[1],x)
+	aabb.min[2] = min(aabb.min[2],y)
+	aabb.min[3] = min(aabb.min[3],z)
+
+	aabb.max[1] = max(aabb.max[1],x)
+	aabb.max[2] = max(aabb.max[2],y)
+	aabb.max[3] = max(aabb.max[3],z)
+
+	aabb.size = aabb.max - aabb.min;
+end
+
+function update!(aabb::AABB, bbcc::AABB)
+	update!(aabb,bbcc.min)
+	update!(aabb,bbcc.max)
+end
+
+
+"""
+	isinside(aabb::AABB,p::Points)
+
+Check if point `p` is in a `aabb`.
+"""
+function isinside(aabb::AABB,p::Common.Point)::Bool
+	return (  p[1]>=aabb.min[1] && p[1]<=aabb.max[1] &&
+			  p[2]>=aabb.min[2] && p[2]<=aabb.max[2] &&
+			   p[3]>=aabb.min[3] && p[3]<=aabb.max[3] )
+end
+
+function makeCubic(aabb::AABB)
+	aabb.max = aabb.min + max(aabb.size...)
+	aabb.size = aabb.max - aabb.min
+end
+
+function Base.show(io::IO, aabb::AABB)
+    println(io, "min: $(aabb.min)")
+	println(io, "max: $(aabb.max)")
+	println(io, "size: $(aabb.size)")
+end
